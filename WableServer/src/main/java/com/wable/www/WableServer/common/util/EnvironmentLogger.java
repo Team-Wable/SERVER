d08848d50271ae0813d6bc22f3446350398e7023
Hong0329 <rhkgkrwkhj@naver.com>
2024-08-08 21:21:34 +0900

    cd test

--- WableServer/src/main/java/com/wable/www/WableServer/common/util/EnvironmentLogger.java
+++ WableServer/src/main/java/com/wable/www/WableServer/common/util/EnvironmentLogger.java
@@ -1,31 +1,33 @@
 package com.wable.www.WableServer.common.util;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.services.s3.endpoints.internal.Value;
 
 @Component
-public class EnvironmentLogger {
+public class EnvironmentLogger implements CommandLineRunner {
 
 	private static final Logger logger = LoggerFactory.getLogger(EnvironmentLogger.class);
 
-	@Autowired
-	private Environment environment;
+	private final Environment environment;
 
-	@PostConstruct
-	public void logEnvironmentVariables() {
+	public EnvironmentLogger(Environment environment) {
+		this.environment = environment;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
 		String dbUrl = environment.getProperty("spring.datasource.url");
 		String dbUser = environment.getProperty("spring.datasource.username");
+		String dbPw = environment.getProperty("spring.datasource.password");
 		String activeProfile = environment.getProperty("spring.profiles.active");
 
-		logger.info("Active Profile: {}", activeProfile);
-		logger.info("Database URL: {}", dbUrl);
-		logger.info("Database Username: {}", dbUser);
-
-		// 원하는 다른 환경 변수들도 로그로 남길 수 있습니다.
+		System.out.println("Active Profile: " + activeProfile);
+		System.out.println("Database URL: " + dbUrl);
+		System.out.println("Database Username: " + dbUser);
+		System.out.println("Database Passeord: " + dbPw);
 	}
 }
