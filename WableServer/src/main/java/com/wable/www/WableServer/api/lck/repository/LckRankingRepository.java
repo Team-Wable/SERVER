e332e97d5ce9dfd137512ae580a56d0df63bc4e4
Hong0329 <rhkgkrwkhj@naver.com>
2024-08-10 17:15:39 +0900

    feat Lck Repository

---
+++ WableServer/src/main/java/com/wable/www/WableServer/api/lck/repository/LckRankingRepository.java
@@ -0,0 +1,7 @@
+package com.wable.www.WableServer.api.lck.repository;
+
+import com.wable.www.WableServer.api.lck.domain.LckRanking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LckRankingRepository extends JpaRepository<LckRanking, Long> {
+}
---
+++ WableServer/src/main/java/com/wable/www/WableServer/api/lck/repository/LckScheduleRepository.java
@@ -0,0 +1,7 @@
+package com.wable.www.WableServer.api.lck.repository;
+
+import com.wable.www.WableServer.api.lck.domain.LckSchedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LckScheduleRepository extends JpaRepository<LckSchedule, Long> {
+}
