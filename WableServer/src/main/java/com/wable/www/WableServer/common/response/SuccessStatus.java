7609e37b952818355d7aacc2a3ef448db1fef0ec
Hong0329 <rhkgkrwkhj@naver.com>
2024-11-12 01:26:15 +0900

    feat : notice get api

--- WableServer/src/main/java/com/wable/www/WableServer/common/response/SuccessStatus.java
+++ WableServer/src/main/java/com/wable/www/WableServer/common/response/SuccessStatus.java
@@ -58,6 +58,7 @@ public enum SuccessStatus {
     POST_GAMESTART_INFONOTIFICATION_SUCCESS(HttpStatus.CREATED,"게임 시작 정보 노티 생성 완료"),
     POST_WEEKDONE_INFONOTIFICATION_SUCCESS(HttpStatus.CREATED,"한 주 종료 정보 노티 생성 완료"),
     NEWS_ALL_SUCCESS(HttpStatus.OK, "뉴스 목록 조회 성공"),
+    NOTICE_ALL_SUCCESS(HttpStatus.OK, "공지사항 목록 조회 성공"),
 
     /**
      * report
