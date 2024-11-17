5e60ddab6d99296393e7bb67dfbd76207c414188
Hong0329 <rhkgkrwkhj@naver.com>
2024-11-18 00:49:28 +0900

    feat : post comment with parentchild api

--- WableServer/src/main/java/com/wable/www/WableServer/api/auth/SocialPlatform.java
+++
@@ -1,7 +0,0 @@
-package com.wable.www.WableServer.api.auth;
-
-public enum SocialPlatform {
-    KAKAO,
-    APPLE,
-    WITHDRAW
-}
--- WableServer/src/main/java/com/wable/www/WableServer/api/member/dto/request/MemberProfilePatchRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.wable.www.WableServer.api.member.dto.request;
-
-public record MemberProfilePatchRequestDto (
-        String nickname,
-        Boolean is_alarm_allowed,
-        String member_intro,
-        String profile_url
-){
-}
