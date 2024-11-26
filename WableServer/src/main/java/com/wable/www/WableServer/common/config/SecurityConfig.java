42cd5c14dd39e6d57d7144db4bd8fa706126f8a4
Hong0329 <rhkgkrwkhj@naver.com>
2024-11-26 19:29:44 +0900

    fix auth whitelist add

--- WableServer/src/main/java/com/wable/www/WableServer/common/config/SecurityConfig.java
+++ WableServer/src/main/java/com/wable/www/WableServer/common/config/SecurityConfig.java
@@ -70,6 +70,7 @@ public class SecurityConfig {
 
     private static final String[] AUTH_WHITELIST = {
             "/api/v1/auth",
+            "/api/v2/auth",
             "/health",
             "/profile",
             "/actuator/health",
