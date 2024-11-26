8a1c80b3d6e1377d50315c24529cf7acac620743
Hong0329 <rhkgkrwkhj@naver.com>
2024-11-26 20:37:22 +0900

    fix member level refine logic

--- WableServer/src/main/java/com/wable/www/WableServer/common/util/MemberUtil.java
+++ WableServer/src/main/java/com/wable/www/WableServer/common/util/MemberUtil.java
@@ -20,39 +20,39 @@ public class MemberUtil {
             return 1;
         } else if (memberExp >= 9 && memberExp <= 23) {
             return 2;
-        } else if (memberExp >= 24 && memberExp <= 47) {
+        } else if (memberExp > 23 && memberExp <= 47) {
             return 3;
-        } else if (memberExp >= 48 && memberExp <= 82) {
+        } else if (memberExp > 47 && memberExp <= 82) {
             return 4;
-        } else if (memberExp >= 83 && memberExp <= 130) {
+        } else if (memberExp > 82 && memberExp <= 130) {
             return 5;
-        } else if (memberExp >= 131 && memberExp <= 194) {
+        } else if (memberExp > 130 && memberExp <= 194) {
             return 6;
-        } else if (memberExp >= 195 && memberExp <= 277) {
+        } else if (memberExp > 194 && memberExp <= 277) {
             return 7;
-        } else if (memberExp >= 278 && memberExp <= 381) {
+        } else if (memberExp > 277 && memberExp <= 381) {
             return 8;
-        } else if (memberExp >= 382 && memberExp <= 508) {
+        } else if (memberExp > 381 && memberExp <= 508) {
             return 9;
-        } else if (memberExp >= 509 && memberExp <= 662) {
+        } else if (memberExp > 508 && memberExp <= 662) {
             return 10;
-        } else if (memberExp >= 663 && memberExp <= 816) {
+        } else if (memberExp > 662 && memberExp <= 816) {
             return 11;
-        } else if (memberExp >= 817 && memberExp <= 970) {
+        } else if (memberExp > 816 && memberExp <= 970) {
             return 12;
-        } else if (memberExp >= 971 && memberExp <= 1124) {
+        } else if (memberExp > 970 && memberExp <= 1124) {
             return 13;
-        } else if (memberExp >= 1125 && memberExp <= 1278) {
+        } else if (memberExp > 1124 && memberExp <= 1278) {
             return 14;
-        } else if (memberExp >= 1279 && memberExp <= 1463) {
+        } else if (memberExp > 1278 && memberExp <= 1463) {
             return 15;
-        } else if (memberExp >= 1464 && memberExp <= 1683) {
+        } else if (memberExp > 1463 && memberExp <= 1683) {
             return 16;
-        } else if (memberExp >= 1684 && memberExp <= 1942) {
+        } else if (memberExp > 1683 && memberExp <= 1942) {
             return 17;
-        } else if (memberExp >= 1943 && memberExp <= 2244) {
+        } else if (memberExp > 1942 && memberExp <= 2244) {
             return 18;
-        } else if (memberExp >= 2245 && memberExp <= 2593) {
+        } else if (memberExp > 2244 && memberExp <= 2593) {
             return 19;
         } else {
             return -1;
