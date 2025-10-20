7fa7c0a8c0db03f2418b6434dc873b9413f2b174
Hong0329 <rhkgkrwkhj@naver.com>
2025-10-20 21:31:22 +0900

    feat curation with title thumbnail

--- WableServer/src/main/java/com/wable/www/WableServer/api/curation/dto/response/CurationGetAllResponseDto.java
+++ WableServer/src/main/java/com/wable/www/WableServer/api/curation/dto/response/CurationGetAllResponseDto.java
@@ -6,12 +6,16 @@ import com.wable.www.WableServer.api.member.domain.Member;
 public record CurationGetAllResponseDto(
 		Long curationId,
 		String curationLink,
+		String curationTitle,
+		String curationThumbnail,
 		String time
 ) {
 	public static CurationGetAllResponseDto of(Curation curation, String time) {
 		return new CurationGetAllResponseDto(
 				curation.getId(),
 				curation.getCurationLink(),
+				curation.getCurationTitle(),
+				curation.getCurationThumbnail(),
 				time
 		);
 	}
